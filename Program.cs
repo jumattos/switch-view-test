50ce128b79de8ea43591e3980ca2a25d5755c8df
Jullyana Mattos Vycas <jumattos@microsoft.com>
2021-01-07 08:14:53 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwitchViewTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
